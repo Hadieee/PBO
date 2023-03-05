ac0d551a8fbaf0293d222a0f3ad6b33afe44a645
Hadieee <hadiepratama15@gmail.com>
2023-03-05 15:19:43 +0800

    New File

---
+++ MLBB/Main.java
@@ -0,0 +1,44 @@
+package MLBB;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+
+        system sys = new system();
+
+        String menu = """
+                <==================================================>
+                <============== Management Hero MLBB ==============>
+                                1. Tambah Hero
+                                2. Lihat Data Hero
+                                3. Ubah Data Hero
+                                4. Hapus Data Hero
+                                5. Keluar
+                <==================================================>\n""";
+        boolean gas = true;
+        while (gas) {
+            System.out.print("\033\143");
+            System.out.print(menu);
+            System.out.print("\n\t\tMasukkan Pilihan : ");
+            int pilihan = Integer.parseInt(br.readLine());
+            switch (pilihan) {
+                case 1: System.out.print("\033\143"); sys.tambah(); break;
+                case 2: sys.lihat(); br.readLine(); System.out.flush(); break;
+                case 3: sys.ubah(); br.readLine(); System.out.flush(); break;
+                case 4: sys.hapus(); br.readLine(); System.out.flush(); break; 
+                case 5: System.out.println("\nTerimakasih telah memakai program kami, Semoga harimu menyenangkan ! :) ");
+                        gas = false; break; 
+
+                default: System.out.println("Pilihan salah, Silahkan coba lagi ! ");
+                         br.readLine();
+                         System.out.flush();
+            }
+        }
+    }
+}
---
+++ MLBB/system.java
@@ -0,0 +1,77 @@
+package MLBB;
+
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class system {
+
+    ArrayList<Hero> list = new ArrayList<Hero>();
+
+    InputStreamReader isr = new InputStreamReader(System.in);
+    BufferedReader br = new BufferedReader(isr);
+
+    public void tambah() throws IOException {
+
+        Hero hero = new Hero();
+
+        hero.tambah();
+        list.add(hero);
+        
+        System.out.println("\nHero " + list.get(list.size() - 1).Nama + " Berhasil Ditambah ! ");
+        br.readLine();
+    }
+
+    public void lihat() throws IOException {
+
+        System.out.print("\033\143");
+        if(list.size() == 0) {
+            System.out.println("Tidak ada Hero disini ! ");
+        }
+        else {
+            System.out.println("""
+                            <===========================================>
+                            <================ List Hero ================>""");
+    
+            for (int i = 0; i < list.size(); i++) {
+                System.out.println("\t\t     <" + (i + 1) + ">");
+                System.out.println("\t" + "Nama Hero       : " + ( list.get(i)).Nama);
+                System.out.println("\t" + "Role Hero       : " +  (list.get(i)).FixRole);
+                System.out.println("\t" + "Spesialist Hero : " + (list.get(i)).FixSpesialist);
+                System.out.println("\t" + "Harga Hero      : " +  (list.get(i)).Harga);
+                System.out.println("\n");
+            }
+        }
+    }
+
+    public void ubah() throws IOException {
+
+        int indeks;
+        Hero hero = new Hero();
+
+        lihat();
+        System.out.print("Masukan Indeks Data Hero Yang Ingin Diubah : ");
+        indeks = Integer.parseInt(br.readLine());
+
+        System.out.println("""
+                            <============================================>
+                            <========== Masukan data hero baru ==========>\n""");
+        hero.tambah();
+
+        list.set(indeks - 1, hero);
+        System.out.println("Data Hero Berhasil Diubah ! ");
+    }
+
+    public void hapus() throws IOException {
+
+        int indeks;
+        lihat();
+
+        System.out.print("Masukan Indeks Data Hero Yang Ingin Dihapus : ");
+        indeks = Integer.parseInt(br.readLine());
+
+        list.remove(indeks - 1);
+        System.out.println("Data Hero Berhasil Dihapus ! ");
+    }
+}
