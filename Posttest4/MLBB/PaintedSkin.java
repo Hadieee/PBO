6416ab3539bbac554436c20e9b1a0b570d3f46ea
Hadieee <hadiepratama15@gmail.com>
2023-03-25 16:19:55 +0800

    Add: Overloading And Overriding

---
+++ Posttest4/MLBB/PaintedSkin.java
@@ -0,0 +1,34 @@
+package MLBB;
+
+public class PaintedSkin extends Skin {
+
+    private String WarnaSkinPainted;
+    private int HargaSkinPainted;
+
+    public int getHargaSkinPainted() {
+        return HargaSkinPainted;
+    }
+
+    public void setHargaSkinPainted(int hargaSkinPainted) {
+        HargaSkinPainted = hargaSkinPainted;
+    }
+
+    public String getWarnaSkinPainted() {
+        return WarnaSkinPainted;
+    }
+
+    public void setWarnaSkinPainted(String warnaSkinPainted) {
+        WarnaSkinPainted = warnaSkinPainted;
+    }
+
+    public int hitungHarga (int harga) {
+        return this.HargaSkinPainted = harga * 1300;
+    }
+
+    public PaintedSkin(String Nama, String Role, String Spesialist, int Harga, String Warna, int hargaSkin,String WarnaSkinPainted, int HargaSkinPainted) {
+        super(Nama, Role, Spesialist, Harga, Warna, hargaSkin);
+        this.WarnaSkinPainted = WarnaSkinPainted;
+        this.HargaSkinPainted = hitungHarga(HargaSkinPainted);
+    }
+
+}
---
+++ Posttest4/MLBB/Skin.java
@@ -0,0 +1,32 @@
+package MLBB;
+
+public class Skin extends Hero {
+    private String warna;
+    private int hargaSkin;
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public int getHargaSkin() {
+        return hargaSkin;
+    }
+
+    public void setHargaSkin(int hargaSkin) {
+        this.hargaSkin = hargaSkin;
+    }
+
+    public int hitungHarga (int harga) {
+        return this.hargaSkin = harga * 1200;
+    }
+
+    public Skin(String Nama, String Role, String Spesialist, int Harga, String warna, int hargaSkin) {
+        super(Nama, Role, Spesialist, Harga);
+        this.warna = warna;
+        this.hargaSkin = hitungHarga(hargaSkin);
+    }
+}
